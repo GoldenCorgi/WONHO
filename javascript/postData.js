5b7e2c779fafc11a24eefb16bc5545eb1811e37a
Wonho <42860714+GoldenCorgi@users.noreply.github.com>
2021-05-10 14:50:06 +0800

    Update postData.js

--- javascript/postData.js
+++ javascript/postData.js
@@ -16,3 +16,24 @@ async function postData(url = '', data = {}) {
     });
     return response.json(); // parses JSON response into native JavaScript objects
 }
+
+// Example POST method for websites with CORS enabled (e.g. Google Apps Script):
+async function postGASData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: 'same-origin', // include, *same-origin, omit
+        // redirect: 'follow', // manual, *follow, error
+        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    // No cors, opaque response
+    return response;
+    // return response.json(); // parses JSON response into native JavaScript objects
+}
